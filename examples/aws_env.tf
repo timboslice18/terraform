f917462674b2bdfdf9100e6c347f2f3ede90827f
timboslice18 <tsurento@gmail.com>
2022-06-04 14:18:41 -0400

    Create aws_env.tf

---
+++ examples/aws_env.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+    region = "us-east-2"
+    access_key = "AKIA33A5FFXS3RJSTFNQ"
+    secret_key = "92szqLC48Bl5q6SwngTwo0oybSzW1FJF3KFrYAFV"
+}
+ 
+ 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+ 
+data "aws_availability_zones" "all" {}
+ 
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+ 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+ 
+#create autoscaling group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+ 
+  min_size = 2
+  max_size = 10
+ 
+#tells it which load balancer resource to use
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+ 
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+ 
+#creates load balancer
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+ 
+ #performs health check
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+ 
+  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+ 
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+output "clb_dns_name" {
+  value       = aws_elb.example.dns_name
+  description = "The domain name of the load balancer"
+  #prints output of dns name
+}
